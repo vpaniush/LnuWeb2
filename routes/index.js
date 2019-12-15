fe2f49c901906a3212ba349115d7f2bc169cac26
vpaniush <volodymyr2507@gmail.com>
2019-12-15 21:46:00 +0200

    configs database for home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var Wine = require('../models/wine');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  Wine.find(function (err, docs) {
+    var wineRows = [];
+    var cols = 3;
+    for (var i = 0; i < docs.length; i += cols) {
+      wineRows.push(docs.slice(i, i + cols));
+    }
+    res.render('index', { title: 'WineMarket', wines: wineRows });
+  });
 });
 
 module.exports = router;
